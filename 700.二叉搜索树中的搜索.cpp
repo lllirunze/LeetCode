464e27d4d2bcf5ab8a2cc95824da7d0b56a41837
St3ve Lee <1927553272@qq.com>
2021-12-06 17:04:41 +0800

    384

---
+++ 700.二叉搜索树中的搜索.cpp
@@ -0,0 +1,73 @@
+/*
+ * @lc app=leetcode.cn id=700 lang=cpp
+ *
+ * [700] 二叉搜索树中的搜索
+ *
+ * https://leetcode-cn.com/problems/search-in-a-binary-search-tree/description/
+ *
+ * algorithms
+ * Easy (76.13%)
+ * Likes:    193
+ * Dislikes: 0
+ * Total Accepted:    102K
+ * Total Submissions: 132.4K
+ * Testcase Example:  '[4,2,7,1,3]\n2'
+ *
+ * 给定二叉搜索树（BST）的根节点和一个值。 你需要在BST中找到节点值等于给定值的节点。 返回以该节点为根的子树。 如果节点不存在，则返回 NULL。
+ * 
+ * 例如，
+ * 
+ * 
+ * 给定二叉搜索树:
+ * 
+ * ⁠       4
+ * ⁠      / \
+ * ⁠     2   7
+ * ⁠    / \
+ * ⁠   1   3
+ * 
+ * 和值: 2
+ * 
+ * 
+ * 你应该返回如下子树:
+ * 
+ * 
+ * ⁠     2     
+ * ⁠    / \   
+ * ⁠   1   3
+ * 
+ * 
+ * 在上述示例中，如果要找的值是 5，但因为没有节点值为 5，我们应该返回 NULL。
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if(root == NULL)
+            return NULL;
+        if(root->val == val)
+            return root;
+        TreeNode *node_left=searchBST(root->left,val);
+        TreeNode *node_right=searchBST(root->right,val);
+        if(node_left!=NULL)
+            return node_left;
+        if(node_right!=NULL)
+            return node_right;
+        return NULL;
+    }
+};
+// @lc code=end
+
