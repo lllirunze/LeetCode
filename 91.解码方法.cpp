5db2189c47a5784a7274e2d6d8792794229d5010
Li Runze <lirunze.me@gmail.com>
2022-11-14 14:14:54 +0800

    动态规划

---
+++ 91.解码方法.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=91 lang=cpp
+ *
+ * [91] 解码方法
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int numDecodings(string s) {
+        int cnt = 0;
+        if(s.size() == 0 || (s.size() == 1 && s[0] == '0')) return 0;
+        if(s.size() == 1) return 1;
+        vector<int> dp(s.size() + 1, 0);
+        dp[0] = 1;
+        for(int i = 0; i < s.size(); ++i){
+            dp[i+1] = s[i] == '0' ? 0 : dp[i];
+            if(i > 0 && (s[i-1] == '1' || (s[i-1] == '2' && s[i] <= '6'))){
+                dp[i+1] += dp[i-1];
+            }
+        }
+        return dp.back();
+    }
+};
+// @lc code=end
+
