84ad5ef20e540417d72dfb13839df66cc45b8a84
Li Runze <lirunze.me@gmail.com>
2022-11-21 12:34:31 +0800

    数组

---
+++ 119.杨辉三角-ii.cpp
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=119 lang=cpp
+ *
+ * [119] 杨辉三角 II
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<int> ans(rowIndex+1,0);
+        ans[0]=1;
+        for(int i = 1 ; i<=rowIndex ; i++){
+            for( int j = i ; j>=1;j--){
+                ans[j] = ans[j]+ans[j-1];
+            }
+        }
+        return ans;
+    }
+};
+// @lc code=end
+
